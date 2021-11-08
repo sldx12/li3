ae5eb09ac2ba5695f5d09ce43bbcb3f20c23fc66
goncalobraga27 <a97541@alunos.uminho.pt>
2021-11-08 20:40:35 +0000

    funcões auxiliares do programa

--- funcoesAux.c
+++ funcoesAux.c
@@ -8,6 +8,7 @@
 // função que confirma se uma dada string contém apenas um número válido
 int nonNegativeInt(char v[]){
     int i = 0, result = 0;
+    //printf("-%s-\n",v);
     while(i < strlen(v)){
         if (isdigit(v[i]) == 0) return result;
         i++;
@@ -48,7 +49,7 @@ int bool_correct(char v[]) {
 
 // função que confirma se uma data é válida
 int check_date(char *d) { // 07-04-2005 <= d <= current_time
-	time_t rawtime;
+    time_t rawtime;
 
     struct tm tm;
     struct tm * timeinfo;
@@ -57,35 +58,36 @@ int check_date(char *d) { // 07-04-2005 <= d <= current_time
     timeinfo = localtime(&rawtime);
 
     memset(&tm, 0, sizeof(tm));
-    strptime(d, "%Y-%m-%d %T", &tm);
+    strptime(d, "%Y-%m-%d %H:%M:%S", &tm);
     int year = tm.tm_year + 1900;
     int mon = tm.tm_mon + 1;
     int day = tm.tm_mday;
     int hour = tm.tm_hour;
     int min = tm.tm_min;
+    int sec= tm.tm_sec;
     int c_year = timeinfo->tm_year + 1900;
     int c_mon = timeinfo->tm_mon + 1;
     int c_day = timeinfo->tm_mday;
     int c_hour = timeinfo->tm_hour;
     int c_min = timeinfo->tm_min;
+    int c_sec = timeinfo->tm_sec;
     
-    if (year > 2005 && year < c_year) return 1;
-    if (year == 2005 && mon > 4) return 1;
-    if (year == c_year && mon < c_mon) return 1;
-    if (year == 2005 && mon == 4 && day >= 7) return 1;
-    if (year == c_year && mon == c_mon && day < c_day) return 1;
-    if (year == c_year && mon == c_mon && day == c_day && hour < c_hour) return 1;
-    if (year == c_year && mon == c_mon && day == c_day &&  hour == c_hour && min < c_min) return 1;
+    if (year > 2005 && year < c_year && mon <= 12 && ((mon == 1 && day>0 && day < 32) || (mon == 2 && day>0 && day < 29) || (mon == 3 && day>0 && day < 32) || (mon == 4 && day>0 && day < 31) || (mon == 5 && day>0 && day < 32) || (mon == 6 && day>0 && day < 31) || (mon == 7 && day>0 && day < 32) || (mon == 8 && day>0 && day < 32) || (mon == 9 && day>0 && day < 31) || (mon == 10 && day>0 && day < 32) || (mon == 11 && day>0 && day < 31) || (mon == 12 && day>0 && day < 32))&& hour>=0 && hour < 24 && min>=0 && min < 60 && sec>=0 && sec <60)  return 1;
+    if (year == 2005 && mon <= 12 && ((mon == 4 && day>=7 && day < 31) || (mon == 5 && day>0 && day < 32) || (mon == 6 && day>0 && day < 31) || (mon == 7 && day>0 && day < 32) || (mon == 8 && day>0 && day < 32) || (mon == 9 && day>0 && day < 31) || (mon == 10 && day>0 && day < 32) || (mon == 11  && day>0 && day < 31) || (mon == 12  && day>0 && day < 32)) && hour >=0 && hour < 24 && min>=0 && min < 60 && sec>=0 && sec < 60) return 1;
+    if (year == c_year && mon < c_mon && (mon == 1 && day>0 && day < 32) && (mon == 2 && day>0 && day < 29) && (mon == 3 && day>0 && day < 32) && (mon == 4 && day>0 && day < 31) && (mon == 5 && day>0 && day < 32) && (mon == 6 && day>0 && day < 31) && (mon == 7 && day>0 && day < 32) && (mon == 8 && day>0 && day < 32) && (mon == 9 && day>0 && day < 31) && (mon == 10 && day>0 && day < 32) && (mon == 11 && day>0 && day < 31) && (mon == 12 && day>0 && day < 32) && hour>=0 && hour < 24 && min >=0 && min < 60 && sec>=0 && sec < 60) return 1;
+    if (year == c_year && mon == c_mon && day < c_day && hour >=0 && hour < 24 && min >=0 && min < 60  && sec>=0 && sec < 60) return 1;
+    if (year == c_year && mon == c_mon && day == c_day && hour < c_hour && min >=0 && min < 60  && sec>=0 && sec< 60) return 1;
+    if (year == c_year && mon == c_mon && day == c_day && hour == c_hour && min < c_min  && sec>=0 && sec< 60) return 1;
+    if (year == c_year && mon == c_mon && day == c_day && hour == c_hour && min < c_min  && sec<c_sec) return 1;
     return 0;
 }
 //Função que vê se uma string é valida
 int string_valida(char v[]){
-    if (strlen(v)==0) return 0;
+    if (v[0]=='\0') return 0;
     else return 1;
 }
 
 //Função que testa se uma lista é valida.Caso seja dá o seu comprimento, caso não seja dá -1 (inacaba)
-/*
 int lista_valida(char v[]){
     v=v+1;// Para retirar o '[';
     int c=0; // Para calcular o comprimento
@@ -96,35 +98,13 @@ int lista_valida(char v[]){
     if (v[0]==']') return 0;
     else{
       while ((temp1=strsep(&temp,","))!=NULL){
-          if (atoi(temp)>=0) c++;
+        //printf("%s\n",temp1);
+        //printf("%d\n",atoi(temp1));
+          if (atoi(temp1)>0) {
+            c++;
+          }
           else return -1;
       }
     }
     return c;
 }
-*/ 
-int lista_valida(char v[]){
-    int i=0;
-    int r=0;
-    if (v[1]==']') return 0;
-    else{
-        while(v[i]!=NULL){
-            if (v[i]==',') r++;
-            i++;
-        }
-    }
-    return r+1;
-}
-
-/*
-A estratégia que eu vou adotar é:
- -Usar um contador de colunas, que vai de 1 até 10
- -criar uma variavel "booliana" para saber se a linha está formatada ou não está 
- -Estratégia do ciclo:
- Enquanto que a fgets for diferente de NULL, o ciclo ocorre;
- Copiar as linhas para um array auxiliar;
- Utilizar a strsep para separar a linha nas colunas que nós queremos 
- Utilizar as funções auxiliares,para testar se aquela coluna está bem formatada.Caso esteja, temos de meter o booleano=1 e aumentar o valor da coluna 
- Caso a coluna se estiver a analisar, tiver desformatada, o booleano=0,e logo de seguida descarta-se essa linha 
- Caso o booleano=1 e a a variavel da coluna for igual a 10 utiliza-se o fprintf para copiar a linha analisada para o ficheiro "OK".
- */
