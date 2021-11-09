94f99c45afe0f2ceda1747c05c8430c343283faa
goncalobraga27 <a97541@alunos.uminho.pt>
2021-11-09 16:04:17 +0000

    Parte 2 do guião

--- exercicio2.c
+++ exercicio2.c
@@ -30,7 +30,7 @@ COMMITS build_commits(char *line){
 int arrayDinamicoU(FILE *s,USER *arrayU){
     int i=0,max_len=400000;
     char buff[max_len];
-    while (fgets(buff,max_len,s)){
+    while (fgets(buff,max_len,s)!= NULL){
         arrayU[i]=build_user(&buff);
         i++;
     }
@@ -45,6 +45,20 @@ int arrayDinamicoR(FILE *s,REPOS *arrayR){
         buff1= strdup(buff);
         if(nonNegativeInt(strdup(strsep(&buff1,";\n")))==1){
         arrayR[i]=build_repos(&buff);
+        i++;
+        }
+    }
+    return i;
+}
+
+int arrayDinamicoC(FILE *s,COMMITS *arrayC){
+    int i=0,max_len=400000;
+    char buff[max_len];
+    while (fgets(buff,max_len,s)){
+        char *buff1;
+        buff1= strdup(buff);
+        if(nonNegativeInt(strdup(strsep(&buff1,";\n")))==1){
+        arrayC[i]=build_commits(&buff);
         i++;
         }
     }
@@ -99,23 +113,121 @@ int giveOwnerID(char v[]){
     r=atoi(&temp);
     return r;
 }
-int verificaIDU(USER *arrayU,int x,int N){
-    int i=0;
-    while (arrayU[i]->id!=x && i<N-1){
-       i++;
+int procuraBinariaU(USER *arrayU,int iI,int iF,int x){
+    int r=0;
+    int meio;
+    while (iI<=iF){
+        meio=(iI+iF)/2;
+        if(arrayU[meio]->id==x) return 1;
+        else if (iI==iF) return 0;
+        else if (arrayU[meio]->id<x) iI=meio+1;
+        else iF=meio;
     }
-    if (arrayU[i]->id==x) return 1;
-    if (i==N-1) return 0;
+    return r;
 }
-int verificaIDR(REPOS *arrayR,int x,int N){
-    int i=0;
-    while (arrayR[i]->id!=x && i<N-1){
-        i++;
+int procuraBinariaR(REPOS *arrayR,int iI,int iF,int x){
+    int r=0;
+    int meio;
+    while (iI<=iF){
+        meio=(iI+iF)/2;
+        if(arrayR[meio]->id==x) return 1;
+        else if (iI==iF) return 0;
+        else if (arrayR[meio]->id<x) iI=meio+1;
+        else iF=meio;
     }
-    if (arrayR[i]->id==x) return 1;
-    if (i==N-1) return 0;
+    return r;
 }
+int procuraBinariaC(COMMITS *arrayC,int iI,int iF,int x){
+    int r=0;
+    int meio;
+    while (iI<=iF){
+        meio=(iI+iF)/2;
+        if(arrayC[meio]->repo_id==x) return 1;
+        else if (iI==iF) return 0;
+        else if (arrayC[meio]->repo_id<x) iI=meio+1;
+        else iF=meio;
+    }
+    return r;
+}
+void quicksortR(REPOS *arrayR, int first, int last) {
+    int i, j, pivot, temp;
+    if (first < last) {
+        pivot = first;
+        i = first;
+        j = last;
 
+        while (i < j) {
+            while ((arrayR[i]->id) <= (arrayR[pivot]->id) && i < last) i++;
+            while ((arrayR[j]->id) > (arrayR[pivot]->id)) j--;
+
+        if (i < j) {
+            temp = arrayR[i]->id;
+            arrayR[i]->id = arrayR[j]->id;
+            arrayR[j]->id = temp;
+            }    
+        }
+
+    temp = arrayR[pivot]->id;
+    arrayR[pivot]->id = arrayR[j]->id;
+    arrayR[j]->id = temp;
+    quicksortR(arrayR, first, j-1);
+    quicksortR(arrayR, j+1, last);
+    
+    }
+}
+void quicksortU(USER *arrayU, int first, int last) {
+    int i, j, pivot, temp;
+    if (first < last) {
+        pivot = first;
+        i = first;
+        j = last;
+
+        while (i < j) {
+            while ((arrayU[i]->id) <= (arrayU[pivot]->id) && i < last) i++;
+            while ((arrayU[j]->id) > (arrayU[pivot]->id)) j--;
+
+        if (i < j) {
+            temp = arrayU[i]->id;
+            arrayU[i]->id = arrayU[j]->id;
+            arrayU[j]->id = temp;
+            }    
+        }
+
+    temp = arrayU[pivot]->id;
+    arrayU[pivot]->id = arrayU[j]->id;
+    arrayU[j]->id = temp;
+    quicksortU(arrayU, first, j-1);
+    quicksortU(arrayU, j+1, last);
+    
+    }
+}
+void quicksortC(COMMITS *arrayC, int first, int last) {
+    int i, j, pivot, temp;
+    if (first < last) {
+        pivot = first;
+        i = first;
+        j = last;
+
+        while (i < j) {
+            while ((arrayC[i]->repo_id) <= (arrayC[pivot]->repo_id) && i < last) i++;
+            while ((arrayC[j]->repo_id) > (arrayC[pivot]->repo_id)) j--;
+
+        if (i < j) {
+            temp = arrayC[i]->repo_id;
+            arrayC[i]->repo_id = arrayC[j]->repo_id;
+            arrayC[j]->repo_id = temp;
+            }    
+        }
+
+    temp = arrayC[pivot]->repo_id;
+    arrayC[pivot]->repo_id = arrayC[j]->repo_id;
+    arrayC[j]->repo_id = temp;
+    quicksortC(arrayC, first, j-1);
+    quicksortC(arrayC, j+1, last);
+    
+    }
+}
+/*
 void exA(FILE *s, FILE *d,USER *arrayU,int Nu){
     int max_len=400000;
     char buff[max_len];
@@ -133,8 +245,8 @@ void exA(FILE *s, FILE *d,USER *arrayU,int Nu){
             buffT1 = strdup(buff);
             AID=giveAuthorID(buffT1);
             printf("%d\n",AID);
-            r1=verificaIDU(arrayU,CID,Nu);
-            r2=verificaIDU(arrayU,AID,Nu);
+            r1=procuraBinariaU(arrayU,0,Nu-1,CID);
+            r2=procuraBinariaU(arrayU,0,Nu-1,AID);
             printf("%d-%d\n",r1,r2);
             if (r1==1 && r2==1) {
                 fprintf(d,buff);
@@ -154,7 +266,7 @@ void exB(FILE *s,FILE *d,REPOS *arrayR,int Nr){
             char *buffT0;
             buffT0=strdup(buff);
             RID=giveReposID(buffT0);
-            r1=verificaIDR(arrayR,RID,Nr);
+            r1=procuraBinariaR(arrayR,0,Nr-1,RID);
             if (r1==1){
                 fprintf(d,buff);
             }
@@ -172,11 +284,68 @@ void exC (FILE *s,FILE *d,USER *arrayU,int Nu){
             char *buffT0;
             buffT0=strdup(buff);
             OWID=giveOwnerID(buffT0);
-            r1=verificaIDU(arrayU,Nu,OWID);
+            r1=procuraBinariaU(arrayU,0,Nu-1,OWID);
             if (r1==1){
                 fprintf(d,buff);
             }
         }
     }
 }
-//Temos de pensar na alinea d) e numa maneira de ordenar o array para melhorar o tempo de execução que esta excessivamente grande 
+*/
+void exAeB(FILE *s, FILE *d,REPOS*arrayR,USER *arrayU,int Nu,int Nr){
+    int max_len=400000;
+    char buff[max_len];
+    int CID, AID,RID,r1,r2,i=0,r3;
+    while (fgets(buff,max_len,s) && i!=2){
+        printf("%s\n",buff);
+        char *buff1;
+        buff1= strdup(buff);
+        if (nonNegativeInt(strdup(strsep(&buff1,";\n")))==1){
+            char *buffT0;
+            buffT0 = strdup(buff);
+            CID=giveCommitterID(buffT0);
+            printf("%d\n",CID);
+            char *buffT1;
+            buffT1 = strdup(buff);
+            AID=giveAuthorID(buffT1);
+            printf("%d\n",AID);
+            char *buffT2;
+            buffT2 = strdup(buff);
+            RID=giveReposID(buffT2);
+            printf("%d\n",RID);
+            r1=procuraBinariaU(arrayU,0,Nu-1,CID);
+            r2=procuraBinariaU(arrayU,0,Nu-1,AID);
+            r3=procuraBinariaR(arrayR,0,Nr-1,RID);
+            printf("%d-%d-%d\n",r1,r2,r3);
+            if (r1==1 && r2==1 && r3==1) {
+                fprintf(d,buff);
+            }
+        }
+        i++;
+    }
+}
+void exCeD (FILE *s,FILE *d,USER *arrayU,COMMITS *arrayC,int Nu,int Nc){
+    int max_len=400000;
+    char buff[max_len];
+    int OWID,RID,r1,r2;
+    while (fgets(buff,max_len,s)){
+        char *buff1;
+        buff1=strdup(buff);
+        if(nonNegativeInt(strdup(strsep(&buff1,";\n")))==1){
+            char *buffT0;
+            buffT0=strdup(buff);
+            OWID=giveOwnerID(buffT0);
+            printf("%d\n",OWID);
+            char *buffT1;
+            buffT1=strdup(buff);
+            RID=giveReposID(buffT0);
+            printf("%d\n",RID);
+            r1=procuraBinariaU(arrayU,0,Nu-1,OWID);
+            r2=procuraBinariaC(arrayC,0,Nc-1,RID);
+            printf("%d-%d\n",r1,r2);
+            if (r1==1 && r2==1){
+                fprintf(d,buff);
+            }
+        }
+    }
+}
