ec4f82c2225317a391ec3f7eec64eb38551d3d4c
goncalobraga27 <a97541@alunos.uminho.pt>
2021-11-10 19:48:18 +0000

    Ficheiro das funções que nos dão ids

---
+++ giveIDs.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "types.h"
+
+int giveID(char v[]){
+    int r;
+    char *buff,*buff1;
+    char temp[15];
+    buff=strdup(v);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    r=atoi(temp);
+    return r;
+}
+
+int giveCommitterID(char v[]){
+    int r;
+    char *buff,*buff1;
+    char temp[15];
+    buff=strdup(v);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    r=atoi(temp);
+    return r;
+}
+
+int giveAuthorID(char v[]){
+    int r;
+    char *buff,*buff1;
+    char temp[15];
+    buff=strdup(v);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    r=atoi(temp);
+    return r;
+}
+int giveReposID(char v[]){
+    int r;
+    char *buff,*buff1;
+    char temp[15];
+    buff=strdup(v);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    r=atoi(temp);
+    return r;
+}
+int giveOwnerID(char v[]){
+    int r;
+    char *buff,*buff1;
+    char temp[15];
+    buff=strdup(v);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    buff1=strdup(strsep(&buff,";"));
+    strcpy(temp,buff1);
+    r=atoi(temp);
+    return r;
+}
